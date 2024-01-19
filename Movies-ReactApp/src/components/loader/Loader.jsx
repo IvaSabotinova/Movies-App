d8974bcbae254027dd61cb7ee073c23ee6d10c54
IvaSabotinova <96121572+IvaSabotinova@users.noreply.github.com>
2024-01-19 10:12:28 +0200

    Fixing loading spinner.

--- Movies-ReactApp/src/components/loader/Loader.jsx
+++ Movies-ReactApp/src/components/loader/Loader.jsx
@@ -1,27 +1,28 @@
 
 function Loader() {
-    return (
+  return (
+    <div
+      style={{
+        position: "fixed",
+        top: "0",
+        left: "0",
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#e2f6fb"
+      }}
+    >
       <div
+        className="spinner-border text-info"
         style={{
-          position: "fixed",
-          top: "0",
-          left: "0",
-          width: "100vw",
-          height: "100vh",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
+          width: "4rem",
+          height: "4rem",
         }}
-      >
-         <div
-          className="spinner-border text-info"
-          style={{
-            width: "4rem",
-            height: "4rem",
-          }}
-        ></div> 
-      </div>
-    );
-  }
-  
-  export default Loader;
+      ></div>
+    </div>
+  );
+}
+
+export default Loader;
